e6f3fd3fd707eeb213d7db9d1b12663b510fe093
CoffeeBon <blue3197@gmail.com>
2017-11-11 20:55:40 +0900

    Lesson 10 - 1 : ArrayList, List<T>(generic collection), foreach

--- AnimalShelter/AnimalShelter/Form1.Designer.cs
+++ AnimalShelter/AnimalShelter/Form1.Designer.cs
@@ -52,6 +52,7 @@
             this.CusNewAddress = new System.Windows.Forms.TextBox();
             this.CusNewDescription = new System.Windows.Forms.TextBox();
             this.CustomerList = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.CusDetailPanel.SuspendLayout();
             this.CusNewPanel.SuspendLayout();
             this.SuspendLayout();
@@ -68,6 +69,7 @@
             // 
             // CusDetailPanel
             // 
+            this.CusDetailPanel.Controls.Add(this.button1);
             this.CusDetailPanel.Controls.Add(this.CusIsQualified);
             this.CusDetailPanel.Controls.Add(this.label2);
             this.CusDetailPanel.Controls.Add(this.CusDescription);
@@ -312,6 +314,16 @@
             this.CustomerList.TabIndex = 4;
             this.CustomerList.Click += new System.EventHandler(this.CustomerList_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(61, 437);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
@@ -356,6 +368,7 @@
         private System.Windows.Forms.Label label11;
         private System.Windows.Forms.Label label15;
         private System.Windows.Forms.ListBox CustomerList;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- AnimalShelter/AnimalShelter/Form1.cs
+++ AnimalShelter/AnimalShelter/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -13,87 +14,33 @@ namespace AnimalShelter
     public partial class Form1 : Form
     {
         //
-        // Customer 클래스 타입의 Cus1 변수는 Form1의 스코프가 끝날때까지 존재한다.
-        public Customer[] CustomerArray = new Customer[10];
-        public int CustomerArrayIndex = 0;
+        // Array를 List로 교체!!
+        public List<Customer> Customers = new List<Customer>();
 
         public Form1()
         {
             InitializeComponent();
         }
-
+        //
+        // 고객 정보 생성 버튼 클릭!
+        //
         private void CreateCustomer_Click(object sender, EventArgs e)
         {
-            #region Create Array
-            //
-            //배열 생성 #1
-            int[] numberArray = new int[5];
-            numberArray[0] = 1;
-            numberArray[1] = 2;
-            numberArray[2] = 3;
-            numberArray[3] = 4;
-            numberArray[4] = 5;
-            //
-            //배열 생성 #2
-            int[] numberArray2 = new int[5] { 1, 2, 3, 4, 5 };
-            //
-            //배열 생성 #3
-            int[] numberArray3 =
-            {
-                1,
-                2,
-                3,
-                4,
-                5
-            };
-            #endregion
-            #region Create Object(or Struct) Array
-            //
-            //오브젝트(클래스) 배열 생성 #1
-            // 오브젝트는 항상 new 키워드를 사용해서 생성자를 만들어야 한다!!!!
-            Customer[] customerArray = new Customer[5];
-            customerArray[0] = new Customer("first", "last", new DateTime(2000, 1, 1));
-            customerArray[1] = new Customer("first", "last", new DateTime(2000, 1, 2));
-            customerArray[2] = new Customer("first", "last", new DateTime(2000, 1, 3));
-            customerArray[3] = new Customer("first", "last", new DateTime(2000, 1, 1));
-            customerArray[4] = new Customer("first", "last", new DateTime(2000, 1, 1));
-            //
-            //오브젝트(클래스) 배열 생성 #2
-            // new 키워드는 반드시 삽입!! 그리고 반드시 ,(콤마)를 찍어준다.
-            Customer[] customerArray2 =
-            {
-                new Customer("first", "last", new DateTime(2000, 1, 1)),
-                new Customer("first", "last", new DateTime(2000, 1, 2)),
-                new Customer("first", "last", new DateTime(2000, 1, 3)),
-                new Customer("first", "last", new DateTime(2000, 1, 4)),
-                new Customer("first", "last", new DateTime(2000, 1, 5)),
-            };
-            #endregion
-            #region Loop
-            //
-            // For Loop syntax
-            int sum = 0;
-            for (int index = 0; index <= 10; index++)
-            {
-                sum += index;
-            }
-            #endregion
-
             DateTime birthday = DateTime.Parse(CusNewBirthday.Text);
 
-            CustomerArray[CustomerArrayIndex] = new Customer(CusNewFirstName.Text, CusNewLastName.Text, birthday)
+            Customer cus = new Customer(CusNewFirstName.Text, CusNewLastName.Text, birthday)
             {
-                //
-                //원래는 cus.Address = "123 Wilshire Blvd.";인데, 
-                //단순화 시킬 수 있음
                 Address = CusNewAddress.Text,
                 Description = CusNewDescription.Text
             };
-            CustomerList.Items.Add(CustomerArray[CustomerArrayIndex].FirstName);
 
-            CustomerArrayIndex++;
+            CustomerList.Items.Add(cus.FirstName);
+
+            Customers.Add(cus);
         }
-
+        //
+        // 오른쪽 창에 정보(Detail)이 보이게 하는 함수.
+        //
         public void ShowDetails(Customer cus)
         {
             CusFullName.Text = cus.FullName;
@@ -113,19 +60,104 @@ namespace AnimalShelter
             }
         }
         //
-        //ListBox에 이름을 클릭하면 그의 정보가 나오게 함!
+        // ListBox에 이름을 클릭하면 그의 정보가 나오게 함!
+        //
         private void CustomerList_Click(object sender, EventArgs e)
         {
             string firstName = CustomerList.SelectedItem.ToString();
 
-            for (int index = 0; index < CustomerArrayIndex; index++)
+            foreach (Customer cus in Customers)
             {
-                if (CustomerArray[index].FirstName == firstName)
+                if (cus.FirstName == firstName)
                 {
-                    ShowDetails(CustomerArray[index]);
+                    ShowDetails(cus);
                     break;
-                } 
+                }
             }
+        }
+        //
+        // ArrayList, List<T>, foreach 예제
+        //
+        private void button1_Click(object sender, EventArgs e)
+        {
+            #region ArrayList
+            //
+            //ArrayList 만들기 #1
+            ArrayList arrayList = new ArrayList
+            {
+                0,  //arrayList[0] = 0;
+                1,  //arrayList[1] = 1;
+                3   //arrayList[2] = 3;
+            };
+            //
+            //ArrayList 만들기 #2
+            ArrayList arrayList2 = new ArrayList();
+            arrayList.Add(0);   //arrayList[0] = 0;
+            arrayList.Add(1);   //arrayList[1] = 1;
+            arrayList.Add(3);   //arrayList[2] = 3;
+            //
+            // 데이터 (인덱스, 값) 삽입
+            arrayList.Insert(2, 2);     //arrayList[] {0, 1, 2, 3};
+            //
+            // 데이터 값 찾아서 삭제 
+            arrayList.Remove(2);  //  arrayList[] {0, 1, 3};
+            //
+            // 데이터 인덱스 찾아서 삭제
+            arrayList.RemoveAt(1);  //  arrayList[] {0, 3};
+            //
+            // summary
+            // 형 변환이 많아질수록 퍼포먼스가 떨어진다!!
+            // ArrayList 클래스는 Object 타입!!
+            // 타입을 지정해주는 일반화 컬렉션(Generic Collection)를 사용 권장. -----> List<T>
+            int sum = 0;
+
+            for (int index = 0; index < arrayList.Count; index++)
+            {
+                int num = (int)arrayList[index];
+                sum += num;
+            }
+            #endregion
+
+            #region Generic Collection
+            //
+            // List<T>에서 T는 Data Type을 의미함. <T>가 들어가있는 것들은 Generic이라 읽는다.
+            // 컬렉션 초기화 방법 #1
+            List<int> intList = new List<int>();
+            intList.Add(1);
+            intList.Add(2);
+            intList.Add(4);
+            //
+            // 컬렉션 초기화 방법 #2
+            List<int> intList2 = new List<int>
+            {
+                1,
+                2
+            };
+            //
+            // Insert, Remove, RemoveAt
+            intList.Insert(2, 3);
+            intList.Remove(2);
+            intList.RemoveAt(0);
+            //
+            // for문을 이용해서 값을 합함
+            int sum2 = 0;
+            for (int index = 0; index < intList.Count; index++)
+            {
+                int value = intList[index];
+                sum2 += value;
+            }
+            #endregion
+
+            #region foreach Loop
+            //
+            // summary
+            // intList라는 컬렉션(배열)에서 하나씩 value 값이 채워진다.
+            // for문과 다르게 index가 없다.
+            foreach (var value in intList)
+            {
+                sum += value;
+            }
+            #endregion
         }
     }
 }
